457b34b703a79de5bc956eba66c23ec5b628e31c
Mahsan Nourani <enchanted.mne@gmail.com>
2018-03-20 10:00:37 +0000

    Final Version without spec

--- rottenpotatoes/features/step_definitions/director_steps.rb
+++ rottenpotatoes/features/step_definitions/director_steps.rb
@@ -2,7 +2,6 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create!(movie)
-    puts movie;
   end
 end
 
@@ -11,17 +10,27 @@ When(/I go to the edit page for "([^"]*)$/) do |movie_title|
     expect(current_path).to eq '/edit'
 end
 
-When (/I fill in "(.*)" with "(.*)$/) do |element, text|
-  puts "************" +  element;
+When (/I fill in "(.*)" with "([^"]*)$/) do |element, text|
   fill_in element.downcase, with: text
 end
 
-When (/I press "(.*)$/) do |element|
+When (/I press "([^"]*)$/) do |element|
     click_on element
 end
 
 Then /^the director of "(.*)" should be "(.*)"$/ do |title_value, director_value|
   movie = Movie.find_by_title(title_value)
-  puts (movie.title + "**" + movie.director)
   expect(movie.director).to eq director_value
+end
+
+When /I follow "([^"]*)$/ do |element|
+  click_on element
+end
+
+Then /I should see "([^"]*)$/ do |text|
+  page.should have_content text
+end
+
+Then /I should not see "([^"]*)$/ do |text|
+  page.should_not have_content text
 end
